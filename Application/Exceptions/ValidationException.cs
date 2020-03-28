5312f8a22b118c4a8dfe8180e70a6328844a283b
Panos Skiadas <panos.skiadas12@gmail.com>
2020-03-28 22:58:15 +0200

    Derived base Exception and implemented custom exception cases

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            : base("One or more validation failures have occured.")
+        {
+            Failures = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(List<ValidationFailure> failures)
+            : this()
+        {
+            var propertyNames = failures
+                .Select(ex => ex.PropertyName)
+                .Distinct();
+
+            foreach (var propertyName in propertyNames)
+            {
+                var propertyFailures = failures
+                    .Where(ex => ex.PropertyName == propertyName)
+                    .Select(ex => ex.ErrorMessage)
+                    .ToArray();
+
+                Failures.Add(propertyName, propertyFailures);
+            }
+        }
+
+        public IDictionary<string, string[]> Failures { get; }
+    }
+}
